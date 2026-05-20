d4124bb93f71d7f7b0b4c494a4a60791e97deb6c
Gbrltarrz <cpe.gutierrez.gabrielluigi@gmail.com>
2026-05-20 21:45:53 +0800

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className ="header-wrapper">
+      <header className ="header-coontainer">
+        <div className = "header-logo-section">
+          <h1 className = "Header-logo">
+            Gabriel Gutierrez
+          </h1>
+
+
+        </div>
+      </header>
+
+    </div>
+    
+    </>
+  )
+}
+export default App
+
