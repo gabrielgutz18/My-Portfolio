d8d7d09e9e91937c0cb1a49bd1e7d3965a5b7573
Gbrltarrz <cpe.gutierrez.gabrielluigi@gmail.com>
2026-05-21 20:25:25 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,32 @@ function App() {
     
       <Header/>
       
-        <div className="hero-pixel-background" aria-hidden="true">
-          <PixelBlast
-            variant="square"
-            className=""
-            style={{}}
-            pixelSize={2}
-            color="#94a3b8"
-            patternScale={1}
-            patternDensity={1.3}
-            enableRipples
-            rippleSpeed={0.3}
-            rippleThickness={0.07}
-            rippleIntensityScale={0.6}
-            speed={0.2}
-            transparent
-            edgeFade={0.2}
-          />
-        </div> 
-        
-
         <section className="hero" id="home">
+          <div className="hero-pixel-background" aria-hidden="true">
+            <PixelBlast
+              variant="square"
+              className=""
+              style={{}}
+              pixelSize={2}
+              color="#b0b7c0"
+              patternScale={1}
+              patternDensity={1.3}
+              enableRipples
+              rippleSpeed={0.3}
+              rippleThickness={0.07}
+              rippleIntensityScale={0.6}
+              speed={0.2}
+              transparent
+              edgeFade={0.2}
+            />
+          </div> 
       
           <div className="hero-image">
             <img src={heroImage} alt="Gabriel" />
             <div className="text-content-hero">
               <h1>Welcome to My Portfolio</h1>
               <h2 className="subtitle">I'm Gabriel, a passionate developer.</h2>
-              <p>This is a showcase of my projects and skills.</p>
+              <p>Computer Engineer with a growing passion for web development, focused on creating clean and user-friendly web experiences.</p>
               <a className="contact-button" href="#contact">Contact Me</a>
             </div>
           </div>
